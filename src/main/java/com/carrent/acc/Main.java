f1fc5454603a09f856a9234eed03b7e258a3048f
PoornachandSarana <poornachand2458@gmail.com>
2024-03-12 16:21:44 -0400

    First Commit - Added Webcrawl and Main class

---
+++ src/main/java/com/carrent/acc/Main.java
@@ -0,0 +1,11 @@
+package com.carrent.acc;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("hello world");
+		Webcrawler.WebCrawlCarRental();
+	}
+
+}
---
+++ src/main/java/com/carrent/acc/Webcrawler.java
@@ -0,0 +1,11 @@
+package com.carrent.acc;
+
+public class Webcrawler {
+	public static void WebCrawlCarRental() {
+		System.out.println("Crawling Carrental");
+	}
+	
+	public static void WebCrawlMomondo() {
+		System.out.println("Crawling Momondo");
+	}
+}
